5c2176cb2538760da29a0056c694a97ad8a549a2
andrew80z <mirror32@gmail.com>
2021-12-21 20:24:52 +0100

    second test

---
+++ cypress/integration/MyProject/CheckMain.js
@@ -0,0 +1,20 @@
+describe('My First Test', () => {
+  it('clicking "type" shows the right headings', () => {
+   const cityName = "Odessa";
+   const countryName = "Germany"
+    cy.visit('/')
+    // Accept cookies
+    cy.get('#sendOpGdpr').click()
+    // Example list of items from top panel
+    let pageList = [ 'WEATHER', 'VIDEOS', 'ALERTS\n29', 'RADAR', 'MAPS', 'SATELLITES', 'MODELS', 'WORLD']
+    // Get list of items from top panel
+    cy.get('.ulMove>li').should(($els) => {
+     // map jquery elements to array of their innerText
+     const elsText = $els.toArray().map(el => el.innerText)
+     expect(elsText).to.deep.eq(pageList)
+
+        })
+    })
+})
+
+
