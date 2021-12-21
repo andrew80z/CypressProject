3ab8ff6c5389f31d4d320f9f30e07998a8e8cf68
andrew80z <mirror32@gmail.com>
2021-12-21 11:09:46 +0100

    Comments added

--- cypress/integration/MyProject/sampleTest.js
+++ cypress/integration/MyProject/sampleTest.js
@@ -1,15 +1,17 @@
 describe('My First Test', () => {
   it('clicking "type" shows the right headings', () => {
     cy.visit('/')
-
+    // Accept cookies
     cy.get('#sendOpGdpr').click()
+    // Enter test city #1
     cy.get('#buscador').type('Odessa')
     cy.contains('Ukraine').click()
     cy.contains('Odessa Weather')
     cy.get('a[title="Odessa"]').should('have.text', 'Odessa')
-    //OPe Maps page
+    //Open Maps page
     cy.contains('Maps').click()
     cy.get('h1').should('have.text','Rain and snow map ')
+    // Enter test city #2
     cy.get('.mapDefName').click()
     cy.contains('Germany').click()
     cy.get('h1').should('have.text', 'Rain and snow map for Germany')
